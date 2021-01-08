68e40522a7dde552311aee834bcaea60cc958d92
Baris Yuksel <brsyuksel@gmail.com>
2021-01-09 00:53:22 +0300

    removing unnecessary new line

--- src/cljs/bilgge_ui/events.cljs
+++ src/cljs/bilgge_ui/events.cljs
@@ -2,8 +2,7 @@
   (:require
    [re-frame.core :as re-frame]
    [bilgge-ui.db :as db]
-   [day8.re-frame.tracing :refer-macros [fn-traced]]
-   ))
+   [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
 (re-frame/reg-event-db
  ::initialize-db
