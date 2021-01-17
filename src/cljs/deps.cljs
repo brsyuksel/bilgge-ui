9a871ea7479cb9d8a294641aecbf29fe606b22fa
Baris Yuksel <brsyuksel@gmail.com>
2021-01-17 17:39:15 +0300

    node-sass and bulma dev deps, jsencrypt dep, css-build script

--- src/cljs/deps.cljs
+++ src/cljs/deps.cljs
@@ -4,4 +4,7 @@
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"
                 "@pact-foundation/pact" "9.13.2"
-                "jsonwebtoken" "8.5.1"}}
+                "jsonwebtoken"          "8.5.1"
+                "node-sass"             "5.0.0"
+                "bulma"                 "0.9.1"}
+ :npm-deps {"jsencrypt" "3.0.0-rc.1"}}
