390d3dd719d79d8b46239983ba8116c713773ec3
Baris Yuksel <brsyuksel@gmail.com>
2021-01-17 18:01:33 +0300

    defining reitit routes

--- src/cljs/bilgge/core.cljs
+++ src/cljs/bilgge/core.cljs
@@ -2,15 +2,21 @@
   (:require
    [reagent.dom :as rdom]
    [re-frame.core :as re-frame]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]
    [bilgge.events :as events]
    [bilgge.views :as views]
-   [bilgge.config :as config]
-   ))
+   [bilgge.config :as config]))
 
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
+
+(def routes
+     [["/" {:name :app-page}]
+      ["/register" {:name :register-page}]
+      ["/login" {:name :login-page}]])
 
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
@@ -21,4 +27,8 @@
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
+      (rfe/start!
+        (rf/router routes)
+        (fn [m] (re-frame/dispatch [::events/set-route-name (-> m :data :name)]))
+        {:use-fragment false})
   (mount-root))
