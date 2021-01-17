a22869a0d87f54b2b5ebd6993b2c7cc9c1d580c9
Baris Yuksel <brsyuksel@gmail.com>
2021-01-17 18:05:24 +0300

    base view implementation, login view and decryption implementation

---
+++ src/cljs/bilgge/utils.cljs
@@ -0,0 +1,21 @@
+(ns bilgge.utils
+    (:require [clojure.string :as string]
+              ["jsencrypt" :as jse]))
+
+(defn clear-rsa-key
+      [priv-key]
+      (-> priv-key
+          (string/replace #"(?m)\n" "")
+          (string/replace #"(?m)-----[A-Z\s]+-----", "")))
+
+(defn decrypt-rsa
+      [enc cipher-text]
+      (if-let [plain (.decrypt enc cipher-text)]
+              plain))
+
+(defn decrypt-rsa-string-key
+      [priv-key cipher-text]
+      (let [enc (jse/JSEncrypt.)]
+           (do
+             (.setPrivateKey enc priv-key)
+             (decrypt-rsa enc cipher-text))))
--- src/cljs/bilgge/views.cljs
+++ src/cljs/bilgge/views.cljs
@@ -1,11 +1,67 @@
 (ns bilgge.views
-  (:require
-   [re-frame.core :as re-frame]
-   [bilgge.subs :as subs]
-   ))
+  (:require [re-frame.core :as re-frame]
+            [bilgge.subs :as subs]
+            [bilgge.login.views :as login.v]))
 
-(defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1 "Hello from " @name]
-     ]))
+(defn header-add-new-item-menu
+      [display?]
+      [:div.column.is-one-fifth
+       (when display?
+             [:div.create-new-wrapper
+        [:div.dropdown.is-hoverable
+         [:div.dropdown-trigger
+          [:button.button.is-light.is-large {:aria-haspopup "true" :aria-controls "create-new-menu"}
+           [:span.icon.is-small
+            [:i.fas.fa-plus {:aria-hidden "true"}]]]]
+         [:div#create-new-menu.dropdown-menu {:role "menu"}
+          [:div.dropdown-content
+           [:div.dropdown-item
+            [:span.icon
+             [:i.fas.fa-sticky-note]]
+            [:span.is-size-5 "note"]]
+           [:div.dropdown-item
+            [:span.icon
+             [:i.fas.fa-keyboard]]
+            [:span.is-size-5 "inputs"]]
+           [:hr.dropdown-divider]
+           [:div.dropdown-item
+            [:span.icon
+             [:i.fas.fa-folder]]
+            [:span.is-size-5 "collection"]]]]]])])
+
+(defn header-brand
+      []
+      [:div.column.is-one-fifth.is-offset-one-fifth
+       [:h1.is-size-1.has-text-centered.has-text-grey-dark.brand-text "bilgge."]])
+
+(defn header-user-actions
+      [display?]
+      [:div.column.is-one-fifth.is-offset-one-fifth
+       (when display?
+             [:div.management-actions
+        [:button.button.is-white.is-large.has-text-grey
+         [:span.icon.is-large
+          [:i.fas.fa-sign-out-alt]]]])])
+
+(defn header
+      [logged-in?]
+      [:section.section.section-header
+       [:div.container
+         [:div.columns
+          [header-add-new-item-menu logged-in?]
+          [header-brand]
+          [header-user-actions logged-in?]]]])
+
+(defn main-panel
+      []
+      (let [route-name @(re-frame/subscribe [::subs/route-name])
+            display-app-menu? (= route-name :app-page)]
+           [:<>
+            [header display-app-menu?]
+            [:section.section
+             [:div.container
+              (case route-name
+                    :login-page [login.v/login-view]
+                    :register-page [:h3 "register-page-view"]
+                    :app-page [:h3 "app-page-view"]
+                    [:div "error."])]]]))
