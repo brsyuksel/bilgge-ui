3d7a0f4dd2bbad4ca09b75281ac673ede743b885
Baris Yuksel <brsyuksel@gmail.com>
2021-01-18 19:02:29 +0300

    random string generator, rsa encryption fn, verifier rsa key pairs

--- src/cljs/bilgge/utils.cljs
+++ src/cljs/bilgge/utils.cljs
@@ -1,10 +1,15 @@
 (ns bilgge.utils
     (:require [clojure.string :as string]
-              ["jsencrypt" :as jse]))
+              ["jsencrypt" :as jse]
+              ["crypto-random-string" :as crs]))
+
+(defn random-string
+      [len]
+      (crs #js {:length len :type "alphanumeric"}))
 
 (defn clear-rsa-key
-      [priv-key]
-      (-> priv-key
+      [key-str]
+      (-> key-str
           (string/replace #"(?m)\n" "")
           (string/replace #"(?m)-----[A-Z\s]+-----", "")))
 
@@ -13,6 +18,10 @@
       (if-let [plain (.decrypt enc cipher-text)]
               plain))
 
+(defn encrypt-rsa
+      [enc plain-text]
+      (.encrypt enc plain-text))
+
 (defn decrypt-rsa-string-key
       [priv-key cipher-text]
       (let [enc (jse/JSEncrypt.)]
@@ -20,8 +29,24 @@
              (.setPrivateKey enc priv-key)
              (decrypt-rsa enc cipher-text))))
 
+(defn encrypt-rsa-string-key
+      [pub-key plain-text]
+      (let [enc (jse/JSEncrypt.)]
+           (do
+             (.setPublicKey enc pub-key)
+             (encrypt-rsa enc plain-text))))
+
 (defn generate-rsa-pairs
       []
       (let [enc (jse/JSEncrypt. #{:default_key_size 1024})]
            (.getKey enc)
            [(.getPrivateKey enc) (.getPublicKey enc)]))
+
+(defn verify-rsa-pairs
+      [prv pub]
+      (let [prv (clear-rsa-key prv)
+            pub (clear-rsa-key pub)
+            plain (random-string 16)]
+           (= plain (->> plain
+                         (encrypt-rsa-string-key pub)
+                         (decrypt-rsa-string-key prv)))))
