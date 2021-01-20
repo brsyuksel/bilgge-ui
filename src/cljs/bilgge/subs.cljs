e747a3109e3ae02da0a93d51e803aeb0de7363ec
Baris Yuksel <brsyuksel@gmail.com>
2021-01-21 00:56:14 +0300

    subs for plain key and plain salt, public and priv keys

--- src/cljs/bilgge/subs.cljs
+++ src/cljs/bilgge/subs.cljs
@@ -8,6 +8,26 @@
    (:token db)))
 
 (re-frame/reg-sub
+  ::private-key
+  (fn [db]
+      (:private-key db)))
+
+(re-frame/reg-sub
+  ::public-key
+  (fn [db]
+      (:public-key db)))
+
+(re-frame/reg-sub
+  ::plain-key
+  (fn [db]
+      (:plain-key db)))
+
+(re-frame/reg-sub
+  ::plain-salt
+  (fn [db]
+      (:plain-salt db)))
+
+(re-frame/reg-sub
   ::route-name
   (fn [db]
       (:route-name db)))
