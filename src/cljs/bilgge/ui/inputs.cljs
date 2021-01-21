a86b2909ba392afa04e110f76c045e7a79679c03
Baris Yuksel <brsyuksel@gmail.com>
2021-01-22 01:35:29 +0300

    large input with placeholder and white bis bg

--- src/cljs/bilgge/ui/inputs.cljs
+++ src/cljs/bilgge/ui/inputs.cljs
@@ -21,6 +21,18 @@
        (when help-text
              [:p.help help-text])])
 
+(defn large-input-with-placeholder-white-bis
+      [value-atom placeholder help-text]
+      [:div.field
+       [:div.control
+        [:input.input.is-large.has-background-white-bis
+         {:type "text"
+          :placeholder placeholder
+          :value @value-atom
+          :on-change #(reset! value-atom (-> % .-target .-value))}]]
+       (when help-text
+             [:p.help help-text])])
+
 (defn labeled-large-file-input
       [value-atom fname-atom label help-text]
       [:div.field
